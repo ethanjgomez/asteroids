fe392733d2c71cf24f5c7c75c9b2551e7e604697
ethanjgomez <ethanjg6@icloud.com>
2025-11-17 18:54:34 -0600

    trippppy duuuuude

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,19 +16,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(
         x=SCREEN_WIDTH / 2,
         y=SCREEN_HEIGHT / 2
     )
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
-
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
